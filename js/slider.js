2a3e5182617b8ddc88358ed95b7ec28064df9896
Chris Pope <cpope13@gmail.com>
2015-01-09 08:25:39 -0800

    Change second page to SEO, RTP, and Calendar

--- js/slider.js
+++ js/slider.js
@@ -66,4 +66,39 @@ $(document).ready(function(){
       var $useCase = $image.prop('name');
       $image.attr('src', '/images/' + $useCase + '.png');
   });
+
+/* -------------- */
+
+  $('.secondpage-firstcolumn-top').hover(
+    function(){
+      var $image = $(this).find('img');
+      var $useCase = $image.prop('name');
+      $image.attr('src', '/images/' + $useCase + '_orange.png');
+  },function(){
+      var $image = $(this).find('img');
+      var $useCase = $image.prop('name');
+      $image.attr('src', '/images/' + $useCase + '.png');
+  });
+
+  $('.secondpage-secondcolumn-top').hover(
+    function(){
+      var $image = $(this).find('img');
+      var $useCase = $image.prop('name');
+      $image.attr('src', '/images/' + $useCase + '_orange.png');
+  },function(){
+      var $image = $(this).find('img');
+      var $useCase = $image.prop('name');
+      $image.attr('src', '/images/' + $useCase + '.png');
+  });
+
+  $('.secondpage-thirdcolumn-top').hover(
+    function(){
+      var $image = $(this).find('img');
+      var $useCase = $image.prop('name');
+      $image.attr('src', '/images/' + $useCase + '_orange.png');
+  },function(){
+      var $image = $(this).find('img');
+      var $useCase = $image.prop('name');
+      $image.attr('src', '/images/' + $useCase + '.png');
+  });
 });
